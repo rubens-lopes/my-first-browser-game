82240357563c939d3ddff4554db9aa99471fd7d0
Rubens Armôa Lopes <rubens.lopes@gmail.com>
2016-12-15 13:52:36 -0300

    must have features

--- js/app.js
+++ js/app.js
@@ -1,39 +1,114 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var enableCollision = true;
+/**
+ *Persona
+ */
+var Persona = function (sprite, x, y) {
+    this.sprite = sprite;
+    this.x = x || 0;
+    this.y = y || 0;
+    this.flip = false;
+    this.flop = false;
+};
+Persona.prototype.render = function () {
+    drawImage(Resources.get(this.sprite), this.x * 101, (this.y * 83) - 35, this.flip, this.flop);
+}
+Persona.prototype.xEdges = function () {
+    return {
+        start: (this.x * 101),
+        end: (this.x * 101) + 101
+    };
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+/**
+ *Enemies
+ */
+var Enemy = function (lane, speed, sprite) {
+    Persona.call(this, sprite || 'images/enemy-bug.png', -1, lane);
+    this.speed = speed || 2;
+    this.flip = Math.random() >= 0.5;
+};
+Enemy.prototype = Object.create(Persona.prototype);
+Enemy.prototype.constructor = Enemy;
+Enemy.prototype.update = function (dt) {
+    if (this.flip) {
+        this.x -= this.speed * dt;
+
+        if (this.x < -1)
+            this.x = 6;
+    }
+    else {
+        this.x += this.speed * dt;
+        
+        if (this.x > 5)
+            this.x = -1;
+    }    
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+/**
+ *Player
+ */
+var Player = function (sprite) {
+    Persona.call(this, sprite, 2, 5);
+    this.score = 0;
+};
+Player.prototype = Object.create(Persona.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.reset = function () {
+    this.x = 2;
+    this.y = 5;
+}
+Player.prototype.update = function () {
+    var p = this;
+    var pXEdges = p.xEdges();
+
+if(enableCollision)    
+    allEnemies.forEach(function (e) {
+        if (p.y === e.y) {
+            var eXEdges = e.xEdges();
+
+            if ((eXEdges.end > pXEdges.start && eXEdges.end < pXEdges.end)
+                || (eXEdges.start > pXEdges.end && eXEdges.start < pXEdges.start)) {
+                    this.score = 0;
+                    p.reset();
+                return;
+               }
+        }
+    });
+        
+    if (p.y === 0) {
+        this.score++;
+        this.reset();
+    }
+};
+Player.prototype.handleInput = function (input) {
+    switch (input) {
+        case 'left':
+            this.x--;
+            break;
+        case 'up':
+            this.y--;
+            break;
+        case 'right':
+            this.x++;
+            break;
+        case 'down':
+            this.y++;
+            break;
+    }
+
+    if (this.x < 0)
+        this.x = 0; 
+    if (this.x > 4)
+        this.x = 4; 
+    if (this.y < 0)
+        this.y = 0;
+    if (this.y > 5)
+        this.y = 5;
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+var player = new Player('images/char-horn-girl.png');
+var allEnemies = [new Enemy(1), new Enemy(2, 3), new Enemy(3, 2)];
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +119,34 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function drawImage(img, x, y, flip, flop, center, deg, width, height) {
+    ctx.save();
+
+    if(typeof width === "undefined") width = img.width;
+    if(typeof height === "undefined") height = img.height;
+    if(typeof center === "undefined") center = false;
+
+    // Set rotation point to center of image, instead of top/left
+    if(center) {
+        x -= width/2;
+        y -= height/2;
+    }
+
+    // Set the origin to the center of the image
+    ctx.translate(x + width/2, y + height/2);
+
+    // Rotate the canvas around the origin
+    var rad = 2 * Math.PI - deg * Math.PI / 180;    
+    ctx.rotate(rad);
+
+    // Flip/flop the canvas
+    if(flip) flipScale = -1; else flipScale = 1;
+    if(flop) flopScale = -1; else flopScale = 1;
+    ctx.scale(flipScale, flopScale);
+
+    // Draw the image    
+    ctx.drawImage(img, -width/2, -height/2, width, height);
+
+    ctx.restore();
+}
--- js/engine.js
+++ js/engine.js
@@ -104,6 +104,7 @@ var Engine = (function(global) {
      * they are just drawing the entire screen over and over.
      */
     function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
@@ -147,11 +148,11 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+        player.render();
+
+        allEnemies.forEach(function (enemy) {
             enemy.render();
         });
-
-        player.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -171,7 +172,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-horn-girl.png'
     ]);
     Resources.onReady(init);
 
